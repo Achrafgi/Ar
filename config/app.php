466ebadcdc25dd0539616b5142d92a35cee3e5af
thangtt 6947 <thangtt6947@co-well.com.vn>
2019-02-18 18:51:49 +0900

    add dev tools: logs-viewer

--- config/app.php
+++ config/app.php
@@ -179,6 +179,7 @@ return [
         Barryvdh\Debugbar\ServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         PrettyRoutes\ServiceProvider::class,
+        Rap2hpoutre\LaravelLogViewer\LaravelLogViewerServiceProvider::class,
     ],
 
     /*
