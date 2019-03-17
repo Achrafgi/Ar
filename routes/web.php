29ddb181c0d974c26bdef0e9e071561124bbd695
tranthethang <tranthethang@gmail.com>
2019-03-17 17:11:05 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -21,5 +10,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('genres', 'GenresController');
-Route::resource('authors', 'AuthorsController');
+Route::middleware(['auth'])->group(function () {
+    Route::resource('genres', 'GenresController');
+    Route::resource('authors', 'AuthorsController');
+});
